277d68def59cd14ec1da910b02bf7d3e6f0b1493
TS14\Goutham <Goutham.Masavarapu@takingshape.com>
2025-09-01 15:28:50 +1000

    ract - usestate

---
+++ dsa/react-check/react-ln/src/counter.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+
+export default function Counter(){
+    const[count,setCount] = useState(0);
+
+    const increment = () => {
+        setCount(prevCount => prevCount + 1);
+    };
+
+    const decrement = () =>{
+        setCount(count -1 )
+    }
+
+
+    return(
+        <div>
+            <p> Count: {count}</p>
+            <button onClick={increment}> + </button>
+            <button onClick={decrement}> - </button>
+        </div>
+    )
+
+
+}
